6eac8f5b998fcf373b6c38fc14cacc711637db55
Timothy <tim87henry@gmail.com>
2020-05-21 23:23:12 +1000

    Added button and code, to reset the sketch pad

---
+++ README.md
@@ -0,0 +1,3 @@
+A basic Sketch Pad in JavaScript. Part of The Odin Project's curriculum.
+
+https://tim87henry.github.io/etch_a_sketch/
--- draw.js
+++ draw.js
@@ -1,11 +1,14 @@
+// This function creates the sketch pad grid and starts coloring 
 function createGrid() {
+    let flexBasisPercent = 100/gridSize;
+    flexBasisPercent = flexBasisPercent+"%";
     for(i=0; i < gridSize; i++) {
         for (j=0; j < gridSize; j++) {
             const div = document.createElement('div');
-            div.style.flexBasis = '6%';
+            div.style.flexBasis = flexBasisPercent;
             container.appendChild(div);
-            div.addEventListener('mouseover', function(e) {
-                div.style.background = 'red';
+            div.addEventListener('mouseover', function() {
+                div.style.background = '#173b67';
             });
         }
     }
@@ -13,4 +16,12 @@ function createGrid() {
 
 let gridSize = 16;
 const container = document.querySelector('#container');
+const restart = document.querySelector('.restart');
 createGrid();
+
+// Function to clear sketch pad and to re-set the pen size
+restart.addEventListener('click', function(){
+    container.innerHTML="";
+    gridSize=prompt("Enter the number of squares, per side, in the sketch pad :");
+    createGrid();
+});
