763935426d71276449cb81c8a884b492479252d9
Timothy <tim87henry@gmail.com>
2020-05-24 14:00:46 +1000

    Added Rainbow mode button and code

--- draw.js
+++ draw.js
@@ -8,20 +8,34 @@ function createGrid() {
             div.style.flexBasis = flexBasisPercent;
             container.appendChild(div);
             div.addEventListener('mouseover', function() {
-                div.style.background = '#173b67';
+                if(rainbowMode) {
+                    div.style.background = 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')';
+                } else {
+                    div.style.background = '#173b67';
+                }
             });
         }
     }
 }
 
 let gridSize = 16;
+let rainbowMode = false;
 const container = document.querySelector('#container');
 const restart = document.querySelector('.restart');
+const rainbow = document.querySelector('.rainbow');
 createGrid();
 
 // Function to clear sketch pad and to re-set the pen size
 restart.addEventListener('click', function(){
     container.innerHTML="";
+    rainbowMode = false;
     gridSize=prompt("Enter the number of squares, per side, in the sketch pad :");
+    createGrid();
+});
+
+// Function to switch to Rainbow pen
+rainbow.addEventListener('click', function(){
+    container.innerHTML="";
+    rainbowMode = true;
     createGrid();
 });
